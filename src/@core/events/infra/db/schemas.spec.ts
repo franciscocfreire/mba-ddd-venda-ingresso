d666624fb1a5d553af7776a722d5f57d2ca37a6b
Francisco Freire <franciscocfreire@gmail.com>
2024-02-20 17:31:43 -0300

    Criacao de campos personalizados no mapeamento

--- src/@core/events/infra/db/schemas.spec.ts
+++ src/@core/events/infra/db/schemas.spec.ts
@@ -9,7 +9,8 @@ test('deve criar um partner', async () => {
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'root'
+        password: 'root',
+        forceEntityConstructor: true,
         
     });
 
